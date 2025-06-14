6e9b066400273f546d557a34e0e987b641840b13
Percy Alejandro Yauri Condori <payauric@unjbg.edu.pe>
2025-06-13 19:57:25 -0500

    Ejercicios de curso de python para reportes de datos

--- default.py
+++ default.py
@@ -2,4 +2,22 @@ import pandas as pd
 import datetime as dt
 import random as rdm
 
-print(pd.__version__)
+#print(pd.__version__)
+
+departamentos = [
+    "Amazonas", "Áncash", "Apurímac", "Arequipa", "Ayacucho", "Cajamarca",
+    "Callao", "Cusco", "Huancavelica", "Huánuco", "Ica", "Junín", "La Libertad",
+    "Lambayeque", "Lima", "Loreto", "Madre de Dios", "Moquegua", "Pasco",
+    "Piura", "Puno", "San Martín", "Tacna", "Tumbes", "Ucayali"
+]
+sucursal = []
+for i in range(50):
+    sucursal.append("sucursal_" + str(i))
+
+# sucursal = [sucursal_1,...,sucursal_10]
+
+laboratorio = [
+    "Laboratorio A", "Laboratorio B", "Laboratorio C", "Laboratorio D",
+    "Laboratorio E", "Laboratorio F", "Laboratorio G", "Laboratorio H",
+    "Laboratorio I", "Laboratorio J"
+]
